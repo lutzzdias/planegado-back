7cd6fa02f8b896e56e6abaffc2b4b5c8d5385e28
RobertoFORTs <bobfortesneto@gmail.com>
2025-09-11 13:57:03 -0400

    chore: adiciona framework gin

--- planegado/main.go
+++ planegado/main.go
@@ -1,7 +1,16 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	log.Info("Iniciando aplicação...")
+
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello, API!"})
+	})
+	r.Run(":8080")
 }
