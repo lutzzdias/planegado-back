f23018e5a95ed52c2bbf7271205cc8c9b8a1e151
lutzzdias <thiagolutzdias@hotmail.com>
2025-09-11 23:16:28 -0400

    chore: add .gitignore + sample calc function

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func calculateInitialInvestment(animalNumber int, weightPrice float64, averageWeight float64) float64 {
+	return (float64(animalNumber) * averageWeight) * weightPrice
+}
+
 func main() {
 	log.Info("App start")
 
 	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, API!"})
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"data": calculateInitialInvestment(10, 12.00, 220)})
 	})
 
 	router.Run(":8080")
